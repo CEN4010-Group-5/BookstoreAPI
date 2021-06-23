aac5de388b769da2c20a8ca0292b95bfda624285
lustered <smellslikelavenders@gmail.com>
2021-06-23 19:29:57 -0400

    Now has routes and Book handling functions

--- components/BookDetails.py
+++ components/BookDetails.py
@@ -1,4 +1,7 @@
+from flask import Flask, request, jsonify
 from BookstoreAPI import db, ma
+from sqlalchemy import exists
+from BookstoreAPI import app
 
 
 # Template class: [4] Book Details
@@ -42,3 +45,43 @@ class Book(db.Model):
         self.Publisher = Pub
         self.YearPublished = Year
         self.Sold = Sold
+
+
+@app.route("/books", methods=["POST"])
+def addBook():
+    # Fetch the POST request's fields
+    Name = request.json["Name"]
+    Description = request.json["Description"]
+    Price = request.json["Price"]
+    Author = request.json["Author"]
+    Genre = request.json["Genre"]
+    Pub = request.json["Publisher"]
+    Year = request.json["YearPublished"]
+    Sold = request.json["Sold"]
+
+    # Check if the book exists in the DB
+    duplicate = db.session.query(exists().where(Book.Name == Name)).scalar()
+
+    if duplicate:
+        return jsonify("Book name is already in the database")
+
+    # Create new book with fetched fields
+    new_book = Book(Name, Description, Price, Author, Genre, Pub, Year, Sold)
+
+    # Only add book if it's unique
+    db.session.add(new_book)
+    db.session.commit()
+
+    # Return new_book as json
+    return new_book.product_schema.jsonify(new_book)
+
+
+@app.route("/books", methods=["GET"])
+def getBooks():
+    # Query
+    all_books = Book.query.all()
+
+    result = Book.products_schema.dump(all_books)
+
+    # Returns all the DB items as json
+    return jsonify(result)
