1eb183aed08c7b34e4c944c77c746c68c3cda764
lustered <smellslikelavenders@gmail.com>
2021-06-26 14:02:58 -0400

    Added functionality to create authors, retrieve authors, get book by ISBN

--- components/views.py
+++ components/views.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from sqlalchemy import exists
 from components.BookDetails import Book
+from components.Author import Author
 from __main__ import db, app
 
 """
@@ -12,9 +13,10 @@ single file, make sure you are naming each function uniquely.
 """
 
 
-# ******************** [4]Book Details ********************
-@app.route("/books", methods=["POST"])
+# ******************** [4] Book Details ********************
+@app.route("/admin/books", methods=["POST"])
 def addBook():
+    """Handles adding a book to the database"""
     # Fetch the POST request's fields
     Name = request.json["Name"]
     Description = request.json["Description"]
@@ -42,8 +44,9 @@ def addBook():
     return new_book.product_schema.jsonify(new_book)
 
 
-@app.route("/books", methods=["GET"])
+@app.route("/admin/books", methods=["GET"])
 def getBooks():
+    """Returns a json with all the books in the database"""
     # Query
     all_books = Book.query.all()
 
@@ -51,6 +54,56 @@ def getBooks():
 
     # Returns all the DB items as json
     return jsonify(result)
+
+
+@app.route("/authors", methods=["GET"])
+def getAuthors():
+    """Returns a json with all the books in the database"""
+    # Query
+    all_authors = Author.query.all()
+
+    result = Author.products_schema.dump(all_authors)
+
+    # Returns all the DB items as json
+    return jsonify(result)
+
+
+@app.route("/admin/createAuthor", methods=["POST"])
+def createAuthor():
+    """Handles adding an author to the database"""
+    # Fetch the POST request's fields
+    FName = request.json["FirstName"]
+    LName = request.json["LastName"]
+    Biography = request.json["Biography"]
+    Publisher = request.json["Publisher"]
+
+    # Check if the book exists in the DB
+    dupFName = db.session.query(exists().where(Author.FirstName == FName)).scalar()
+    dupLName = db.session.query(exists().where(Author.LastName == LName)).scalar()
+
+    if dupFName and dupLName:
+        return jsonify("Author is already in the database.")
+
+    # Create new book with fetched fields
+    new_author = Author(FName, LName, Biography, Publisher)
+
+    # Only add book if it's unique
+    db.session.add(new_author)
+    db.session.commit()
+
+    # Return new_book as json
+    return new_author.product_schema.jsonify(new_author)
+
+
+@app.route("/books/<ISBN>", methods=["GET"])
+def getBookByISBN(ISBN):
+    """Returns the book requested by the specific ISBN route"""
+    book = Book.query.get(ISBN)
+
+    if book is None:
+        return jsonify(None)
+
+    return Book.product_schema.jsonify(book)
 
 
 # ******************** [4]Book Details ********************
