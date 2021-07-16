31cad2354b451824467a365f185de487fc9c6931
lustered <smellslikelavenders@gmail.com>
2021-07-16 16:05:27 -0400

    Fixed branch structure

--- components/views.py
+++ components/views.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from sqlalchemy import exists
 from components.BookDetails import Book
 from components.Author import Author
-from components.Wishlist import Wishlist
+from components.Wishlist import wishList
 from __main__ import db, app
 
 """
@@ -133,8 +133,6 @@ def getBooksByAuthor(AUTHOR):
 # ******************** [4] Book Details ********************
 
 # ******************** [4] Wishlist ************************
-
-
 @app.route("/wishList/createWishList", methods=["POST"])
 def addWishlist():
     # Fetch the POST request's fields
@@ -143,15 +141,16 @@ def addWishlist():
     # Check if the wishlist title already exists
     duplicate = db.session.query(exists().where(wishList.Title == Title)).scalar()
 
-    if duplicate: 
+    if duplicate:
         return jsonify("Wishlist tile already in use.")
 
     new_Wish = wishList(Title)
-    
+
     db.session.add(new_Wish)
     db.session.commit()
 
     return new_Wish.product_schema.jsonify(new_Wish)
+
 
 @app.route("/wishList/<title>", methods=["POST"])
 def addBook(title):
@@ -168,8 +167,6 @@ def addBook(title):
 
     someList = wishList.query.filter_by(Title=title).first()
 
-
-   
     # Create new book with fetched fields
     new_book = Book(Name, Description, Price, Author, Genre, Pub, Year, Sold)
     new_book.ownerId = someList.id
@@ -178,16 +175,17 @@ def addBook(title):
     db.session.commit()
 
     # Return new_book as json
-    return new_book.product_schema.jsonify(new_book)    
+    return new_book.product_schema.jsonify(new_book)
+
 
 @app.route("/wishList/<title>", methods=["GET"])
 def getBookInList(title):
     """Returns the books requested from a wishlist."""
     wish = wishList.query.filter_by(Title=title).first()
 
-
     if wish is None:
         return jsonify(None)
 
     return wishList.product_schema.jsonify(wish)
+
 # ******************** [4] Wishlist ************************
