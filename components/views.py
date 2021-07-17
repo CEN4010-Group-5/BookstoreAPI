c9cd01c6fa0ad7377c438e5a0cfefdb69d03d2de
Lint Action <lint-action@samuelmeuli.com>
2021-07-17 17:17:42 +0000

    Fix code style issues with Black

--- components/views.py
+++ components/views.py
@@ -289,28 +289,28 @@ def addWishlist():
 
 # @app.route("/wishList/<title>", methods=["POST"])
 # def addBook(title):
-    # """Handles adding a book to the wishlist."""
-    # # Fetch the POST request's fields
-    # Name = request.json["Name"]
-    # Description = request.json["Description"]
-    # Price = request.json["Price"]
-    # Author = request.json["Author"]
-    # Genre = request.json["Genre"]
-    # Pub = request.json["Publisher"]
-    # Year = request.json["YearPublished"]
-    # Sold = request.json["Sold"]
+# """Handles adding a book to the wishlist."""
+# # Fetch the POST request's fields
+# Name = request.json["Name"]
+# Description = request.json["Description"]
+# Price = request.json["Price"]
+# Author = request.json["Author"]
+# Genre = request.json["Genre"]
+# Pub = request.json["Publisher"]
+# Year = request.json["YearPublished"]
+# Sold = request.json["Sold"]
 
-    # someList = Wishlist.query.filter_by(Title=title).first()
+# someList = Wishlist.query.filter_by(Title=title).first()
 
-    # # Create new book with fetched fields
-    # new_book = Book(Name, Description, Price, Author, Genre, Pub, Year, Sold)
-    # new_book.ownerId = someList.id
-    # # Only add book if it's unique
-    # db.session.add(new_book)
-    # db.session.commit()
+# # Create new book with fetched fields
+# new_book = Book(Name, Description, Price, Author, Genre, Pub, Year, Sold)
+# new_book.ownerId = someList.id
+# # Only add book if it's unique
+# db.session.add(new_book)
+# db.session.commit()
 
-    # # Return new_book as json
-    # return new_book.product_schema.jsonify(new_book)
+# # Return new_book as json
+# return new_book.product_schema.jsonify(new_book)
 
 
 @app.route("/wishList/<title>", methods=["GET"])
