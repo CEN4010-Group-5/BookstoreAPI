aa32f02eea9dc6b041dfb6768bdfe4657a1b34af
lustered <smellslikelavenders@gmail.com>
2021-07-17 13:17:05 -0400

    Updated database and commented out not working code

--- components/views.py
+++ components/views.py
@@ -271,6 +271,7 @@ def getBooksByLimit(LIMIT):
 def addWishlist():
     # Fetch the POST request's fields
     Title = request.json["Title"]
+    Books = request.json["Books"]
 
     # Check if the wishlist title already exists
     duplicate = db.session.query(exists().where(Wishlist.Title == Title)).scalar()
@@ -278,7 +279,7 @@ def addWishlist():
     if duplicate:
         return jsonify("Wishlist tile already in use.")
 
-    new_Wish = Wishlist(Title)
+    new_Wish = Wishlist(Title, Books)
 
     db.session.add(new_Wish)
     db.session.commit()
@@ -286,30 +287,30 @@ def addWishlist():
     return new_Wish.product_schema.jsonify(new_Wish)
 
 
-@app.route("/wishList/<title>", methods=["POST"])
-def addBook(title):
-    """Handles adding a book to the wishlist."""
-    # Fetch the POST request's fields
-    Name = request.json["Name"]
-    Description = request.json["Description"]
-    Price = request.json["Price"]
-    Author = request.json["Author"]
-    Genre = request.json["Genre"]
-    Pub = request.json["Publisher"]
-    Year = request.json["YearPublished"]
-    Sold = request.json["Sold"]
+# @app.route("/wishList/<title>", methods=["POST"])
+# def addBook(title):
+    # """Handles adding a book to the wishlist."""
+    # # Fetch the POST request's fields
+    # Name = request.json["Name"]
+    # Description = request.json["Description"]
+    # Price = request.json["Price"]
+    # Author = request.json["Author"]
+    # Genre = request.json["Genre"]
+    # Pub = request.json["Publisher"]
+    # Year = request.json["YearPublished"]
+    # Sold = request.json["Sold"]
 
-    someList = Wishlist.query.filter_by(Title=title).first()
+    # someList = Wishlist.query.filter_by(Title=title).first()
 
-    # Create new book with fetched fields
-    new_book = Book(Name, Description, Price, Author, Genre, Pub, Year, Sold)
-    new_book.ownerId = someList.id
-    # Only add book if it's unique
-    db.session.add(new_book)
-    db.session.commit()
+    # # Create new book with fetched fields
+    # new_book = Book(Name, Description, Price, Author, Genre, Pub, Year, Sold)
+    # new_book.ownerId = someList.id
+    # # Only add book if it's unique
+    # db.session.add(new_book)
+    # db.session.commit()
 
-    # Return new_book as json
-    return new_book.product_schema.jsonify(new_book)
+    # # Return new_book as json
+    # return new_book.product_schema.jsonify(new_book)
 
 
 @app.route("/wishList/<title>", methods=["GET"])
