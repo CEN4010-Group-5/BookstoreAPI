3523c7a2c9b0bcd71555cedc0448e9be6e7dd0c0
Ren-13 <86617623+Ren-13@users.noreply.github.com>
2021-07-21 13:53:27 -0400

    Update Wishlist.py

--- components/Wishlist.py
+++ components/Wishlist.py
@@ -1,11 +1,8 @@
-from __main__ import db, ma, app
-
-
-class Wishlist(db.Model):
+class wishList(db.Model):
     # Schema
     class ProductSchema(ma.Schema):
         class Meta:
-            fields = ("Title", "Books")
+            fields = ("Title")
 
     # Create DB fields
     Title = db.Column(db.String(300), primary_key=True)
@@ -15,6 +12,24 @@ class Wishlist(db.Model):
     product_schema = ProductSchema()
     products_schema = ProductSchema(many=True)
 
-    def __init__(self, Title, Books):
+    def __init__(self, Title):
         self.Title = Title
-        self.Books = Books
+
+class wishBooks(db.Model):
+    """This class represents a book author"""
+
+    # Schema
+    class ProductSchema(ma.Schema):
+        class Meta:
+            fields = ("bookName")
+
+    # Create DB fields
+    
+    bookName = db.Column(db.String(300), unique=True)
+
+    product_schema= ProductSchema()
+    products_schema= ProductSchema(many = True)
+
+    def __init__(self, bookName):
+        self.bookName = bookName
+    
