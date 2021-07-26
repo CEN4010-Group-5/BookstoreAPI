f24cb5d56eef5a38b333795ecc0b2d6294772e56
Ren-13 <86617623+Ren-13@users.noreply.github.com>
2021-07-25 20:35:35 -0400

    Update views.py

--- components/views.py
+++ components/views.py
@@ -4,7 +4,6 @@ from sqlalchemy import exists
 from components.BookDetails import Book
 from components.Author import Author
 from components.Wishlist import Wishlist
-from components.Wishlist import Wishbooks
 from components.Profile import Profile
 from components.Profile import CreditCards
 from __main__ import db, app
