463464d93630466da4bcb77c9dea86e674112ff2
GerarLeon64 <86471845+GerarLeon64@users.noreply.github.com>
2021-07-26 11:48:36 -0400

    Update ShoppingCart.py

--- components/ShoppingCart.py
+++ components/ShoppingCart.py
@@ -12,8 +12,7 @@ class ShoppingCart(db.Model):
 
     # Create DB fields
     User = db.Column(db.String(300), primary_key=True)
-    Books = db.Column(db.Integer())
-
+    Books = db.Column(db.String(300))
     # Product schema for single and multiple items
     product_schema = ProductSchema()
     products_schema = ProductSchema(many=True)
@@ -21,3 +20,19 @@ class ShoppingCart(db.Model):
     def __init__(self, User, Books):
         self.User = User
         self.Books = Books
+    
+    def addBookToShoppingCart(self, ISBN):
+        self.Books = self.Books + ISBN
+        return "Book " + Book.query.get(int(ISBN)).Name + " has been added"
+
+    def deleteBookFromShoppingCart(self, ISBN):
+        newListAfterDeletion = ""
+        for book in self.Books:
+            if book != ISBN:
+                newListAfterDeletion = newListAfterDeletion + book
+        if newListAfterDeletion == self.Books:
+            return "Book to be deleted was not found in shopping cart"
+        else:
+            self.Books = newListAfterDeletion
+            return "Book " + Book.query.get(int(ISBN)).Name + " has been deleted"
+
