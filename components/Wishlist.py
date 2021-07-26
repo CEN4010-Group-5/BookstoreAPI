8f7e2b6fbecd8ffc1fdcc22686699932df52c06b
Lint Action <lint-action@samuelmeuli.com>
2021-07-26 15:57:29 +0000

    Fix code style issues with Black

--- components/Wishlist.py
+++ components/Wishlist.py
@@ -5,18 +5,18 @@ class Wishlist(db.Model):
     # Schema
     class ProductSchema(ma.Schema):
         class Meta:
-            fields = ("Title")
+            fields = "Title"
 
     # Create DB fields
     Title = db.Column(db.String(300), primary_key=True)
-    
+
     # Product schema for single and multiple items
     product_schema = ProductSchema()
     products_schema = ProductSchema(many=True)
 
     def __init__(self, Title):
         self.Title = Title
-        
+
 
 class WishBooks(db.Model):
     class ProductSchema(ma.Schema):
@@ -24,11 +24,10 @@ class WishBooks(db.Model):
             fields = ("Books", "listId")
 
     Books = db.Column(db.String(300), unique=True)
-    listId =db.Column(db.String(300), db.ForeignKey("wishList.Title"))
+    listId = db.Column(db.String(300), db.ForeignKey("wishList.Title"))
 
     product_schema = ProductSchema()
     products_schema = ProductSchema(many=True)
 
     def __init__(self, Books):
         self.Books = Books
-        
